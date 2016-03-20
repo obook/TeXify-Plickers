dee440235fa6ce82d1995397cc6afcf8f8920ad2
obook <obooklage@free.fr>
2016-03-20 14:34:02 +0100

    Update TeXify-Plickers.user.js

--- TeXify-Plickers.user.js
+++ TeXify-Plickers.user.js
@@ -1,28 +1,32 @@
 // ==UserScript==
 // @name        TeXify-Plickers
 // @namespace   https://github.com/obook/TeXify-Plickers
+// @version     2
 // @description GreaseMonkey script for add LaTeX code in Plickers website. Use delimiters [; and ;]
 // @author         obooklage
 // @oujs:author    obooklage
 // @include        http://plickers.com/*
 // @include        https://plickers.com/*
-// @version     1
+// @downloadURL    https://github.com/obook/TeXify-Plickers/
+// @updateURL      https://github.com/obook/TeXify-Plickers/raw/master/TeXify-Plickers.user.js
+// @icon           https://github.com/obook/TeXify-Plickers/raw/master/icon.png
 // @grant       none
+// @run-at document-end
 // ==/UserScript==
 
-console.log("TeXify-Plickers is loading ...");
 var mathjaxloaded = false;
 
 function OnLoadMathJax()
 {
-	console.log("OnLoadMathJax was called.");
+    var startTime = new Date();
+	console.log('TeXify-Plickers MathJax loaded.' + startTime.toLocaleTimeString());
 	mathjaxloaded = true;
 	MathJax.Hub.Config({tex2jax: {inlineMath: [['[;',';]']]}});
 }
 
 function TeXifyPlickers() {
     var startTime = new Date();
-    console.log('TeXify-Plickers ' + startTime.toLocaleTimeString());
+    console.log('TeXify-Plickers MathJax rescan ' + startTime.toLocaleTimeString());
     if( mathjaxloaded == true)
     {
     	MathJax.Hub.Typeset();
@@ -32,6 +36,8 @@ function TeXifyPlickers() {
 /* Application */
 
 if (self == top) { /* run only in the top frame. we do our own frame parsing */
+    var startTime = new Date();
+    console.log('TeXify-Plickers is started ' + startTime.toLocaleTimeString());
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML";
@@ -39,6 +45,3 @@ if (self == top) { /* run only in the top frame. we do our own frame parsing */
     document.head.appendChild(script);
     setInterval(TeXifyPlickers, 5000);
 }
-
-console.log("TeXify-Plickers is loaded.");
-
