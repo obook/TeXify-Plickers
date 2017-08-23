9bbecc2390bbbb4d0c36747b0fd4a2cb8cd180c9
obook <obooklage@free.fr>
2017-08-23 15:22:07 +0200

    new WebExtension

---
+++ WebExtension/content_scripts/index.js
@@ -0,0 +1,13 @@
+/*
+The index.js script has no access to any document or any window object.
+index.js is the main add-on or background script.
+Its execution environment and context are very specific to the addon SDK.
+Specific javascript must be in a Content Script
+*/
+
+console.log('FF-TeXify-Plickers index.js');
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL("data/texifyplickers.js");
+(document.head||document.documentElement).appendChild(s);
+s.parentNode.removeChild(s);
